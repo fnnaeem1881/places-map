3faa0f2917b7fb700160e7f6246e6d9388f90b36
Mehedi Hasan <mehedinaeem66@gmail.com>
2025-04-28 17:24:52 +0600

    modify search

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const placeRoutes = require('./routes/placeRoutes');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,11 @@ app.use(express.json()); // Middleware to parse JSON requests
 
 // Define routes
 app.use('/api/places', placeRoutes);
+app.get('/', (req, res) => {
+  console.log('index route hit');
+  
+  res.sendFile(path.join(__dirname, './views/index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
